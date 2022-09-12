e0761e62a0a04ab9ef79f5e2f117db3a9be9517e
Maxim Marshak <bormaxi1980@gmail.com>
2022-09-12 19:41:12 +0600

    Initial commit (script release 1)

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # github-starred-repos-loader
-Shell script for pull all your starred GitHub repositories in specifield folder
+
+Shell script for pull all your starred GitHub repositories in specifield folder.
+
+## Purposes:
+
+This script helps for pull all your GitHub starred repositories locally.
+
+This is typically needed in a situation when you have many stared repositories to clone locally in a folder from remote repositories, such as collection of various utilities, and you want to create a "clear" repositories store from latest versions.
+
+
+
+This script does simple things:
+
+>> Gets list of your starred GitHub repositories.
+>>
+>> git clone in a specified folder
+>> 
+>> View clone statistics
+
+## Thats all!
+
+*It works on GitHub only! (not in GitLab or BitBucket)
+
+## Important!
+
+If you have a lot of starred GitHub repositories, and they are big, clone operations may take a long time to complete.
+
+## Usage:
+
+- Clone this repository
+- Run 'bash pull.sh ', aka this, and wait:
+
+>> bash pull.sh "/Volumes/Transcend/repos" "<YOUR GITHUB TOKEN>"
+
+- Bingo!
+
+
+
+![alt text](./repos.png "Repos List in Finder")
+
+## Notes:
+
+### How to get your GitHub personal API access token for API:
+
+[Link](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token)
+
+### More about GitHub API:
+
+[Link](https://docs.github.com/en/rest)
+
+## More about GitHub Starred API:
+
+[Link](https://docs.github.com/en/rest/activity/starring)
+
+### More About GitHub API Pagination Requests:
+
+[Link](https://docs.github.com/en/rest/guides/traversing-with-pagination)
+
+![alt text](./pagination_headers.png "Pagination Headers")
+
+## TODO:
+
+- Add excluded repos list
+- Remember last cloned repository / log cloned repositories 
+- More than 1000 starred repos?
---
+++ pull.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# https://github.com/bormaxi8080/github-starred-repos-loader.git
+
+#set -e
+
+if [ ${#@} -lt 2 ]; then
+    echo "usage: $0 [DESTINATION PATH] [YOUR GITHUB TOKEN]"
+    exit 1;
+fi
+
+# destination repositories folder
+# repository="/Volumes/Transcend/repos"
+DESTINATION_PATH=$1
+GITHUB_TOKEN=$2
+
+echo "This is script for pull all your GitHub starred repositories to specified folder"
+echo "Destination folder: $DESTINATION_PATH"
+echo ""
+
+#GITHUB_API_HEADER_ACCEPT="Accept: application/vnd.github.v3+json"
+GITHUB_API_HEADER_ACCEPT="Accept: application/vnd.github+json"
+
+GITHUB_API_REST="/user/starred"
+
+# remember current path
+CURRENT_PATH="$PWD"
+
+# set repositories counter
+COUNTER=0
+
+temp=`basename $0`
+rm -rf ${temp}.tmp
+
+TMPFILE=`mktemp ./${temp}.tmp` || exit 1
+
+function rest_call {
+    curl -s $1 -H "${GITHUB_API_HEADER_ACCEPT}" -H "Authorization: token $GITHUB_TOKEN" | grep "clone_url" | sed -e 's/"clone_url": //g; s/,//g; s/"//g; s/ //g' >> $TMPFILE
+
+    while IFS= read -r repo
+    do
+      echo "Cloning $repo"
+      echo ""
+      cd $DESTINATION_PATH
+      git clone $repo
+      echo ""
+      cd $CURRENT_PATH
+      let "COUNTER+=1"
+    done < ./${temp}.tmp
+}
+
+# single page result-s (no pagination), have no link: section, the grep result is empty
+# curl -s -I "https://api.github.com${GITHUB_API_REST}" -H "${GITHUB_API_HEADER_ACCEPT}" -H "Authorization: token $GITHUB_TOKEN"
+last_page=`curl -s -I "https://api.github.com${GITHUB_API_REST}" -H "${GITHUB_API_HEADER_ACCEPT}" -H "Authorization: token $GITHUB_TOKEN" | grep '^link:' | sed -e 's/^link:.*page=//g' -e 's/>.*$//g'`
+
+# does this result use pagination?
+if [ -z "$last_page" ]; then
+    # no - this result has only one page
+    rest_call "https://api.github.com${GITHUB_API_REST}"
+else
+    # yes - this result is on multiple pages
+    for p in `seq 1 $last_page`; do
+        echo "Page: $p of $last_page"
+        echo ""
+        rest_call "https://api.github.com${GITHUB_API_REST}?page=$p"
+    done
+fi
+
+echo "$COUNTER repos cloned"
+echo "Done"
